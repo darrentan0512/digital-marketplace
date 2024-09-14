60698d8be2444eb98e0bf3ea7b430466df6cf172
darrentan <darren.tan0512@gmail.com>
2024-09-15 00:49:01 +0800

    feat: sso auth service

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,27 +1,48 @@
 import Link from "next/link";
 import { NavbarLinks } from "./NavbarLinks";
 import { Button } from "@/components/ui/button";
+
+import {
+  LoginLink,
+  RegisterLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { MobileMenu } from "./MobileMenu";
 
-export function Navbar() {
-    return (
-        <nav className="relative max-w-7xl w-full flex md:grid md:grid-cols-12 items-center px-4 md:px-8 mx-auto py-7">
-            <div className="md:col-span-3">
-                <Link href="/">
-                    <h1 className="text-2xl font-semibold">Marshal<span className="text-primary">UI</span></h1>
-                </Link>
-            </div>
-            <NavbarLinks />
+export async function Navbar() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
 
-            <div className="flex items-center gap-x-2 ms-auto md:col-span-3">
-                <Button>Login</Button>
-                <Button variant="secondary">Register</Button>
+  console.log(user);
+  return (
+    
+    <nav className="relative max-w-7xl w-full flex md:grid md:grid-cols-12 items-center px-4 md:px-8 mx-auto py-7">
+      <div className="md:col-span-3">
+        <Link href="/">
+          <h1 className="text-2xl font-semibold">
+            Marshal<span className="text-primary">UI</span>
+          </h1>
+        </Link>
+      </div>
+      <NavbarLinks />
 
-                <div className="md:hidden">
-                    <MobileMenu />
-                </div>
+      <div className="flex items-center gap-x-2 ms-auto md:col-span-3">
+        {user ? (
+          <h1> Welcome {user.given_name} </h1>
+        ) : (
+          <div className="flex items-center gap-x-2">
+            <Button>
+              <LoginLink>Login</LoginLink>
+            </Button>
+            <Button variant="secondary">
+              <RegisterLink>Register</RegisterLink>
+            </Button>
+            <div className="md:hidden">
+              <MobileMenu />
             </div>
-            
-        </nav>
-    )
+          </div>
+        )}
+      </div>
+    </nav>
+  );
 }
