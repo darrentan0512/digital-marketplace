dceca38a4f3c9fe803f5c55b5962fa575170ba65
darrentan <darren.tan0512@gmail.com>
2024-09-28 00:53:06 +0800

    feat: add in file upload ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "./components/Navbar";
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
+import { extractRouterConfig } from "uploadthing/server";
+import { ourFileRouter } from "./api/uploadthing/core";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +22,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+      <NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)}
+        />
         <Navbar />
         {children}
       </body>
--- app/sell/page.tsx
+++ app/sell/page.tsx
@@ -1,9 +1,12 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { SelectCategory } from "../components/SelectCategory";
 import { Textarea } from "@/components/ui/textarea";
 import { TipTapEditor } from "../components/Editor";
+import { UploadButton } from "@uploadthing/react";
+import { UploadDropzone } from "../lib/uploadthing";
+import { Button } from "@/components/ui/button";
 
 export default function SellRoute() {
     return (
@@ -29,16 +32,27 @@ export default function SellRoute() {
                         </div>
                         <div className="flex flex-col gap-y-2">
                             <Label>Small Summary</Label>
-                            <Textarea placeholder="Please describe your product"/>
+                            <Textarea placeholder="Please describe your product" />
                         </div>
                         <div className="flex flex-col gap-y-2">
                             <Label>Description</Label>
                             <TipTapEditor />
                         </div>
+                        <div className="flex flex-col gap-y-2">
+                            <Label>Product Images</Label>
+                            <UploadDropzone endpoint="imageUploader" />
+                        </div>
+                        <div className="flex flex-col gap-y-2">
+                            <Label>Product Images</Label>
+                            <UploadDropzone endpoint="productFileUpload" />
+                        </div>
                     </CardContent>
+                    <CardFooter className="mt-5">
+                        <Button> Submit form </Button>
+                    </CardFooter>
                 </form>
             </Card>
-            
+
         </section>
     )
 }
